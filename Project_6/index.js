96169f69c9cfc27cac7ca6b893179298aca431c0
FuChih <t19887348@hotmail.com>
2021-10-21 23:21:21 +0800

    Project_6

--- Project_6/index.js
+++ Project_6/index.js
@@ -1,13 +1,37 @@
-function filter() {
+/*
+const btn = document.querySelector('#btn')
+btn.addEventListener('click', () => {
+  wordFilter()
+})
+
+function wordFilter() {
   const input = document.querySelector('#input')
   const output = document.querySelector('#output')
   const badWords = ['fuck', 'shit', 'cunt', 'bitch', 'damn', 'asshole']
 
   badWords.forEach((el) => {
-    const word = new RegExp(el, 'ig')
-    input.value = input.value.replace(word, '*')
+    const regex = new RegExp(el, 'ig')
+    input.value = input.value.replace(regex, '****')
   })
 
   output.value = input.value
   input.value = ''
+}
+*/
+
+// ------------ same as above -----------
+const btn = document.querySelector('#btn')
+btn.addEventListener('click', () => {
+  const input = document.querySelector('#input')
+
+  wordFilter(input.value)
+  input.value = ''
+})
+
+function wordFilter(inputValue) {
+  const output = document.querySelector('#output')
+  const badWords = ['fuck', 'shit', 'cunt', 'bitch', 'damn', 'asshole']
+  const regex = new RegExp(badWords.join('|'), 'gi')
+
+  output.value = inputValue.replace(regex, '****')
 }
