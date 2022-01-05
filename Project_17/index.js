ef046ff49f095466096827e6b1ea7ad742526205
FuChih <t19887348@hotmail.com>
2022-01-05 08:19:28 +0800

    Project_17

---
+++ Project_17/index.js
@@ -0,0 +1,17 @@
+const originBox = document.querySelector('.origin-box')
+const zoomRiver = document.querySelector('.zoom-river')
+const magnifier = document.querySelector('.magnifier')
+const zoomBox = document.querySelector('.zoom-box')
+
+originBox.addEventListener('mousemove', (e) => {
+  zoomBox.style.display = 'block'
+  magnifier.style.top = e.clientY - magnifier.clientHeight / 2 + 'px'
+  magnifier.style.left = e.clientX - magnifier.clientWidth / 2 + 'px'
+  zoomRiver.style.top = -(e.clientY - magnifier.clientHeight / 2) * 10 + 'px'
+  zoomRiver.style.left = -(e.clientX - magnifier.clientWidth / 2) * 10 + 'px'
+  console.log()
+})
+
+originBox.addEventListener('mouseout', (e) => {
+  zoomBox.style.display = 'none'
+})
